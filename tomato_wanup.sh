c6e6b6b3bab9a11a4bc813985c03d21608431b3c
theredhood13 <rxt00@hotmail.com>
2019-03-04 13:21:03 +0800

    Update tomato_wanup.sh

--- tomato_wanup.sh
+++ tomato_wanup.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-mkdir /var/wwwext/
-sleep 30
-wget http://tomato-design.dot26.sk/red/custom.css -P /var/wwwext/
+wget https://raw.githubusercontent.com/theredhood13/tomato-design/master/release/dark/custom.css -P /var/wwwext/
