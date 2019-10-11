2bdfddfe77a5d3f8d448da084597391bf67693b2
tsg2k2 <41454537+tsg2k2@users.noreply.github.com>
2019-10-11 18:23:27 -0400

    Update ColumnEnabler.user.js

--- release/ColumnEnabler.user.js
+++ release/ColumnEnabler.user.js
@@ -1,14 +1,27 @@
 // ==UserScript==
 // @name         Column-based layout helper
 // @namespace    https://github.com/tsg2k2
-// @version      0.5
+// @version      0.6
 // @description  Enables column-based layout and and applies DOM fixes
 // @author       tsg2k2
-// @include      http://192.168.1.1/
-// @include      http://freshtomato/
-// @include      http://homerouter/
+// @include      http*://192.168.1.*/$
+// @include      http*://freshtomato/
+// @include      http*://homerouter/
 // @include      */basic-network.asp
 // @include      */advanced-wireless.asp
+// @include      */advanced-ctnf.asp
+// @include      */advanced-firewall.asp
+// @include      */advanced-adblock.asp
+// @include      */advanced-routing.asp
+// @include      */qos-settings.asp
+// @include      */nas-ftp.asp
+// @include      */nas-samba.asp
+// @include      */vpn-pptp-server.asp
+// @include      */admin-access.asp
+// @include      */admin-tomatoanon.asp
+// @include      */admin-log.asp
+// @include      */admin-sched.asp
+// @include      */admin-access.asp
 // @grant        none
 // ==/UserScript==
 
