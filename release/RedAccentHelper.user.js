abb81b605cdb1ebda82af6c062c4006e74181bdc
tsg2k2 <41454537+tsg2k2@users.noreply.github.com>
2020-06-29 18:15:47 -0400

    Update RedAccentHelper.user.js

--- release/RedAccentHelper.user.js
+++ release/RedAccentHelper.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Red accent helper
 // @namespace    https://github.com/tsg2k2
-// @version      0.2
+// @version      0.3
 // @description  Enables column-based layout and applies DOM fixes
 // @author       tsg2k2
 // @include      /^https?:\/\/192\.168\.\d+\.\d+.+$/
 // @include      /^https?://freshtomato/?$/
 // @include      /^https?://homerouter/?$/
+// @run-at       document-body
 // @grant        none
 // ==/UserScript==
 
