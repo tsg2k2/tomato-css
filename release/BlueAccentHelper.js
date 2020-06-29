2742b9c1a3f3a3ed319439675e02615580ba051a
tsg2k2 <41454537+tsg2k2@users.noreply.github.com>
2020-06-29 18:03:15 -0400

    Create BlueAccentHelper.js

---
+++ release/BlueAccentHelper.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Blue accent helper
+// @namespace    https://github.com/tsg2k2
+// @version      0.2
+// @description  Enables column-based layout and applies DOM fixes
+// @author       tsg2k2
+// @include      /^https?:\/\/192\.168\.\d+\.\d+.+$/
+// @include      /^https?://freshtomato/?$/
+// @include      /^https?://homerouter/?$/
+// @grant        none
+// ==/UserScript==
+
+
+
+(function() {
+    'use strict';
+    let targetNode = document.body;
+    if (targetNode) {
+        let style = '--tomato-accent-color: #2180de;';
+        targetNode.style = style;
+    }
+})();
